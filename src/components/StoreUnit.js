c0a860406ae212043c5e5586c4e4887df8986366
Marwanx10 <marwan.mohsen1000@gmail.com>
2022-01-01 17:03:13 +0200

    fix store unit for same bugs

--- src/components/StoreUnit.js
+++ src/components/StoreUnit.js
@@ -21,16 +21,33 @@ class StoreUnit extends Observer {
 
   update(input) {
     if (input) {
-      this.dataMap.set(input['address'], {
-        latency: input['latency'],
-        value: input['value'],
-      });
+      var { addr, val } = input;
+      var data = this.dataMap.get(addr);
+      if (data) {
+        data.push({
+          latency: this.latency,
+          value: val,
+        });
+        this.dataMap.set(addr, data);
+      } else {
+        this.dataMap.set(addr, [
+          {
+            latency: this.latency,
+            value: val,
+          },
+        ]);
+      }
     }
     var write = false;
-    for (var [address, { latency, value }] of this.dataMap) {
-      let l = latency - 1;
-      if (l <= 0 && !write) {
-        this.output.load(this.memoryUnit.map.set(address, value));
+    for (var [address, values] of this.dataMap) {
+      let index = 0;
+      for (var [latency, value] of values) {
+        latency--;
+        if (l <= 0 && !write) {
+          this.output.load(this.memoryUnit.map.set(address, value));
+          values.pop(index);
+        }
+        index++;
       }
     }
   }
